518afbf13ab3dc4eb39172ef6d3159a1b97b4cff
Alexey Voronov <voronoff2803@gmail.com>
2023-10-07 18:27:48 +0300

    Initial Commit

---
+++ vorobei-sui-3/vorobei_sui_3App.swift
@@ -0,0 +1,17 @@
+//
+//  vorobei_sui_3App.swift
+//  vorobei-sui-3
+//
+//  Created by Alexey Voronov on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct vorobei_sui_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
